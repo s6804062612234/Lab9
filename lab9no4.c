1c4f5b6bb8a42a561db59de82675e8d5ff94d5f1
Thananon Sananrum <s6804062612234@email.kmutnb.ac.th>
2026-02-10 15:11:48 +0700

    lab9no4.c:swap array1d

--- lab9no4.c
+++ lab9no4.c
@@ -12,6 +12,28 @@ int main() {
 	printf("Before swap function: a=%d, b=%d, c=%d\n", a, b, c);
 	swapValue(&a, &b, &c);
 	printf("After swap function: a= %d, b=%d, c=%d\n", a, b, c);
+	
+	
+	int x[] = {1, 2, 3};
+	int y[] = {4, 5, 6};
+	
+	printf("\nBefore swapArray:\n");
+	for(i=0; i<3; i++) {
+		printf("%d ", x[i]);
+	}
+	printf("\n");
+	for(i=0; i<3; i++) {
+		printf("%d ", y[i]);
+	}
+	swapArray(x, y, 3);
+	printf("\nAfter swapArray:\n");
+	for(i=0; i<3; i++) {
+		printf("%d ", x[i]);
+	}
+	printf("\n");
+	for(i=0; i<3; i++) {
+		printf("%d ", y[i]);
+	}
 }
 
 
@@ -21,4 +43,14 @@ void swapValue(int *a, int *b, int *c) {
 	*a = *b;
 	*b = *c;
 	*c = temp;
+}
+
+
+void swapArray(int *a, int *b, int size) {
+	int temp, i;
+	for(i=0; i<size; i++) {
+		temp = a[i];
+		a[i] = b[i];
+		b[i] = temp;
+	}
 }
