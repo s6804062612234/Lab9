cd9b4fd333b2a4f4b70564eb9f03f25b1319e35e
Thananon Sananrum <s6804062612234@email.kmutnb.ac.th>
2026-02-23 03:37:19 +0700

    lab9no5.c: x Matrices

--- lab9no4.c
+++ lab9no4.c
@@ -1,56 +1,47 @@
 #include <stdio.h>
+#define NMAX 10
 
+//functions prototype
+void inputArray(int array[], int N);
+void showArray2D(int matrix[][10], int N);
+void constructMatrix(int P[][10], int N, int A[ ], int B[ ]);
 
-//function prototype
-void swapValue(int *a, int *b, int *c);
-void swapArray(int a[], int b[], int size); 
-//or void swapArray(int *a, int *b, int size); 
-
-
-int main() {
-	int a = 1, b = 2, c = 3, i;
-	printf("Before swap function: a=%d, b=%d, c=%d\n", a, b, c);
-	swapValue(&a, &b, &c);
-	printf("After swap function: a= %d, b=%d, c=%d\n", a, b, c);
-	
-	
-	int x[] = {1, 2, 3};
-	int y[] = {4, 5, 6};
-	
-	printf("\nBefore swapArray:\n");
-	for(i=0; i<3; i++) {
-		printf("%d ", x[i]);
-	}
-	printf("\n");
-	for(i=0; i<3; i++) {
-		printf("%d ", y[i]);
-	}
-	swapArray(x, y, 3);
-	printf("\nAfter swapArray:\n");
-	for(i=0; i<3; i++) {
-		printf("%d ", x[i]);
-	}
-	printf("\n");
-	for(i=0; i<3; i++) {
-		printf("%d ", y[i]);
-	}
+int main() {  
+   int a[NMAX], b[NMAX], p[NMAX][NMAX], n;
+   printf("Enter N = ");           
+   scanf("%d", &n);
+   printf("Input array A \n");     
+   inputArray(a, n);
+   printf("Input array B \n");
+   inputArray(b, n);
+   constructMatrix(p, n, a, b);
+   printf("Matrix P \n");
+   showArray2D(p, n);   
+   return 0;
+}
+//functions definition
+void inputArray(int array[], int N) {
+    int i;
+    for(i=0; i<N; i++) {
+        scanf("%d", &array[i]);
+    }
 }
 
-
-void swapValue(int *a, int *b, int *c) {
-	int temp;
-	temp = *a;
-	*a = *b;
-	*b = *c;
-	*c = temp;
+void showArray2D(int matrix[][10], int N) {
+    int i, j;
+    for(i=0; i<N; i++) {
+        for(j=0; j<N; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
 }
 
-
-void swapArray(int *a, int *b, int size) {
-	int temp, i;
-	for(i=0; i<size; i++) {
-		temp = a[i];
-		a[i] = b[i];
-		b[i] = temp;
-	}
+void constructMatrix(int P[][10], int N, int A[ ], int B[ ]) {
+    int i, j;
+    for(i=0; i<N; i++) {
+        for(j=0; j<N; j++) {
+            P[i][j] = A[i] * B[j];
+        }
+    }
 }
